ba7189be06a7b68a6ff12e11d2edf5221867c0e4
Grishenkov Leonid <grishenkovleonid@gmail.com>
2024-02-27 23:30:03 +0300

    Added scripts

--- scripts/deb.sh
+++ scripts/deb.sh
@@ -1,9 +1,11 @@
 export CONFIGS_PATH=$HOME/code/configs
 
+export ZSH_CUSTOM=
+
 # Upgrade apt and install main utilities
 sudo apt update \
     && sudo apt upgrade -y \
-    && sudo apt install -y zsh lf git ripgrep wget curl bat exa stow
+    && sudo apt install -y zsh lf git ripgrep wget curl bat exa stow fzf jq
 
 # Change default shell for $USER
 chsh -s $(which zsh)
@@ -16,8 +18,29 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/the
 		&& git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting \
 		&& git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-ln -sfiv $CONFIGS_PATH/zsh/deb/.zshrc $HOME/.zshrc \
-		&& mkdir -pv $ZSH_CUSTOM/plugins/vi-mode && ln -sfiv $CONFIGS_PATH/oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh $ZSH_CUSTOM/plugins/vi-mode/vi-mode.plugin.zsh \
-		&& mkdir -pv $HOME/.config/lf && ln -sfiv $CONFIGS_PATH/lf/lfrc $HOME/.config/lf/lfrc \
-		&& mkdir -pv $HOME/.config/nvim && for file in $CONFIGS_PATH/nvim/*; do ln -sfiv $file $HOME/.config/nvim/$(basename $file); done
+# 
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+# git-ignore
+# https://github.com/laggardkernel/git-ignore
+git clone https://github.com/laggardkernel/git-ignore.git $ZSH_CUSTOM/plugins/git-ignore
+
+# zsh-help
+# https://github.com/Freed-Wu/zsh-help
+git clone https://github.com/Freed-Wu/zsh-help.git $ZSH_CUSTOM/plugins/zsh-help
+
+# fast-syntax-highlighting
+# https://github.com/zdharma-continuum/fast-syntax-highlighting
+git clone -q https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZSH_CUSTOM/plugins/fast-syntax-highlighting
+
+git clone https://github.com/trystan2k/zsh-tab-title $ZSH_CUSTOM/plugins/zsh-tab-title
+
+
+# Install poetry 
+# https://python-poetry.org/docs/#installing-with-the-official-installer
+sudo curl -sSL https://install.python-poetry.org \
+    | sudo POETRY_HOME=/opt/poetry python3 -
+sudo ln -sf /opt/poetry/bin/poetry /usr/local/bin/poetry
+
+
 
--- scripts/macos.sh
+++ scripts/macos.sh
@@ -1,12 +1,6 @@
 export CONFIGS_PATH=$HOME/code/configs
 
 
-# Get Meslo Nerd Fonts
-# https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/Meslo
-mkdir -pv $HOME/Library/Fonts/meslo-nerd-fonts \
-		&& wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Meslo.tar.xz -O $HOME/Library/Fonts/meslo-nerd-fonts/Meslo.tar.xz \
-		&& tar -xvf $HOME/Library/Fonts/meslo-nerd-fonts/Meslo.tar.xz \
-		&& rm $HOME/Library/Fonts/meslo-nerd-fonts/Meslo.tar.xz
 
 mkdir -pv /Users/leonidgrisenkov/.oh-my-zsh/custom/plugins/vi-mode && ln -sfiv /Users/leonidgrisenkov/code/configs/oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh /Users/leonidgrisenkov/.oh-my-zsh/custom/plugins/vi-mode/vi-mode.plugin.zsh
 
