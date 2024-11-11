47b4e5d20a058cd5547ebffc4eb1be3039dfb9e7
Grishenkov Leonid <grishenkovleonid@gmail.com>
2024-11-11 09:05:15 +0300

    Add nvim plugin: Avante

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,25 @@
+return {
+    {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        version = false, -- set this if you want to always pull the latest change
+        opts = {
+            -- add any opts here
+        },
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        build = "make",
+        config = function()
+            require("avante").setup({
+                provider = "claude",
+                claude = {
+                    endpoint = "https://api.anthropic.com",
+                    model = "claude-3-5-sonnet-20241022",
+                    temperature = 0,
+                    max_tokens = 4096,
+                },
+                hints = { enabled = false},
+            })
+        end,
+    },
+}
