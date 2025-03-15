a1338fbd591d42cd1da88be6c21a9b3cecb115db
Grishenkov Leonid <grishenkovleonid@gmail.com>
2025-03-15 21:14:37 +0300

    chore(nvim): Enable avante; Regular plugins updates

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -1,8 +1,9 @@
 return {
     {
+        -- https://github.com/yetone/avante.nvim
         "yetone/avante.nvim",
         event = "VeryLazy",
-        enabled = false,
+        enabled = true,
         lazy = false,
         version = false, -- set this if you want to always pull the latest change
         -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -12,9 +13,9 @@ return {
                 provider = "claude",
                 claude = {
                     endpoint = "https://api.anthropic.com",
-                    model = "claude-3-5-sonnet-20241022",
+                    model = "claude-3-7-sonnet-20250219",
                     temperature = 0,
-                    max_tokens = 4096,
+                    max_tokens = 10000,
                 },
                 hints = { enabled = false },
             })
