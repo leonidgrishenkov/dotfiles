e82a873a13079dd2e5bbbbbbbbeb2db188abed0c
Grishenkov Leonid <grishenkovleonid@gmail.com>
2025-03-24 13:34:44 +0300

    feat(nvim): add a few autocmds

--- nvim/.config/nvim/lua/config/autocmd.lua
+++ nvim/.config/nvim/lua/config/autocmd.lua
@@ -2,19 +2,56 @@ local function augroup(name)
     return vim.api.nvim_create_augroup("LocalAutoCmd" .. name, { clear = true })
 end
 
--- Strip trailing spaces before write
--- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
---     group = augroup("strip_space"),
---     pattern = { "*" },
---     callback = function()
---         vim.cmd([[ %s/\s\+$//e ]])
---     end,
--- })
-
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = augroup("highlight_yank"),
     callback = function()
         vim.highlight.on_yank()
+    end,
+})
+
+-- Resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+    group = augroup("resize_splits"),
+    callback = function()
+        local current_tab = vim.fn.tabpagenr()
+        vim.cmd("tabdo wincmd =")
+        vim.cmd("tabnext " .. current_tab)
+    end,
+})
+
+-- Close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("close_with_q"),
+    pattern = {
+        "checkhealth",
+        "help",
+        "lspinfo",
+        "notify",
+        "spectre_panel",
+        "startuptime",
+    },
+    callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.schedule(function()
+            vim.keymap.set("n", "q", function()
+                vim.cmd("close")
+                pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+            end, {
+                buffer = event.buf,
+                silent = true,
+                desc = "Quit buffer",
+            })
+        end)
+    end,
+})
+
+-- Wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("wrap_spell"),
+    pattern = { "text", "plaintex", "gitcommit", "markdown" },
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
     end,
 })
