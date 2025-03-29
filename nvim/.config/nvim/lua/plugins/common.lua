c1c752d4712d813750ef37d4ccc340524d309d1b
Grishenkov Leonid <grishenkovleonid@gmail.com>
2025-03-29 14:55:39 +0300

    refactor(nvim): move dressing into common.lua module

--- nvim/.config/nvim/lua/plugins/common.lua
+++ nvim/.config/nvim/lua/plugins/common.lua
@@ -12,4 +12,18 @@ return {
             vim.g.startuptime_tries = 10
         end,
     },
+    {
+        --[[
+        That plugin improves UI and enables telescope plugin
+        for two neovim builtin functions: `vim.ui.select()` and `vim.ui.input()`.
+        When one of that functions called from any plugin improved version will be shown.
+
+        Repo: https://github.com/stevearc/dressing.nvim
+
+        Docs: :h dressing-configuration
+        --]]
+        "stevearc/dressing.nvim",
+        event = "VeryLazy",
+        opts = {},
+    },
 }
