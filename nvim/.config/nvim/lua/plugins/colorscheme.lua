a355e73f482671e1feaa4c7451361acab585271e
Grishenkov Leonid <grishenkovleonid@gmail.com>
2025-08-27 13:23:46 +0300

    fix(nvim): set auto integrations to avoid win color differences

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,15 +5,14 @@ return {
         tag = "v1.10.0",
         opts = {
             flavour = "frappe", -- Can be one of: latte, frappe, macchiato, mocha
-            transparent_background = true, -- disables setting the background color.
+            transparent_background = true,
             term_colors = false,
             show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
             no_italic = false, -- Force no italic
             no_bold = false, -- Force no bold
             no_underline = false, -- Force no underline
+            auto_integrations = true,
             integrations = {
-                rainbow_delimiters = true,
-                blink_cmp = true,
                 native_lsp = {
                     enabled = true,
                     -- To see all available values: :h highlight-args
