928656f27fb488a5f1790d14a310682a1a9fcba5
Grishenkov Leonid <grishenkovleonid@gmail.com>
2025-10-11 21:03:19 +0300

    refactor(zsh): removed extra eza aliases

--- zsh/.config/zsh/conf/aliases.zsh
+++ zsh/.config/zsh/conf/aliases.zsh
@@ -2,8 +2,6 @@ alias k="kubectl"
 alias d="docker"
 alias t="terraform"
 
-alias ls="eza --all --oneline --icons --group-directories-first"
-alias ll="eza --all --long --icons --group-directories-first --created --modified --header --binary --time-style long-iso"
-alias tree="eza --tree --all --icons --group-directories-first --ignore-glob='.git*|.venv*|__pycache__*|.DS_store'"
+alias ls="eza --icons=auto --group-directories-first --time-style=long-iso"
 
 alias v=$EDITOR
