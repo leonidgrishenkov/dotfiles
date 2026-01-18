e70fe75b056e32172806f13b4ad9fa76654891b2
Grishenkov Leonid <grishenkovleonid@gmail.com>
2026-01-18 22:25:29 +0300

    feat(nvim): add smear-cursor plug

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -47,4 +47,13 @@ return {
             },
         },
     },
+    {
+        -- https://github.com/sphamba/smear-cursor.nvim
+        "sphamba/smear-cursor.nvim",
+        opts = {
+            stiffness = 0.5,
+            trailing_stiffness = 0.5,
+            matrix_pixel_threshold = 0.5,
+        },
+    },
 }
