b3f9ef87e405b8633741518eea3627629b85bde7
Grishenkov Leonid <grishenkovleonid@gmail.com>
2026-01-31 16:15:02 +0300

    feat(nvim): lock plugs; removed smear-cursor plug

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -47,13 +47,4 @@ return {
             },
         },
     },
-    {
-        -- https://github.com/sphamba/smear-cursor.nvim
-        "sphamba/smear-cursor.nvim",
-        opts = {
-            stiffness = 0.5,
-            trailing_stiffness = 0.5,
-            matrix_pixel_threshold = 0.5,
-        },
-    },
 }
