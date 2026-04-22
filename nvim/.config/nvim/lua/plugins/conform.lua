1071daac1b81ec84dd6c75a367b9892b266bdac9
Leonid Grishenkov <grishenkovleonid@gmail.com>
2026-04-22 21:58:24 +0300

    feat(nvim): set cli options for sqlfluff in order to fix sql file in any case; lock plugs

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,13 @@ return {
             formatters_by_ft = {
                 yaml = { "yamlfmt" },
             },
+            formatters = {
+                sqlfluff = {
+                    -- Pass --FIX-EVEN-UNPARSABLE and --ignore=templating explicitly so sqlfluff formats the file
+                    -- even when template variables (like jinja params, macros, etc.) cannot be resolved at lint time.
+                    args = { "fix", "--FIX-EVEN-UNPARSABLE", "--ignore=templating", "-" },
+                },
+            },
         },
     },
 }
