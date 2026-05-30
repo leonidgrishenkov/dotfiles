9a2e9281747e28f3037e75e83c9c66db14e584a5
Leonid Grishenkov <grishenkovleonid@proton.me>
2026-05-31 01:21:45 +0300

    feat(raycast): add scripts folder and example how to use shell script in order to sync obsidian notes via rclone

---
+++ raycast/.local/share/raycast/scripts/sync-obsidian.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Sync Obsidian
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.author grishenkovleonid
+# @raycast.authorURL https://raycast.com/grishenkovleonid
+
+cd ~/Filen && task sync-obsidian
