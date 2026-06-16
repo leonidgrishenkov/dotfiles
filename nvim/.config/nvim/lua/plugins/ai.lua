ce6a6312b23154e6d06b247875eb988699952d9f
Leonid Grishenkov <grishenkovleonid@proton.me>
2026-06-17 00:20:41 +0300

    add

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -23,7 +23,7 @@ return {
                     openai_compatible = {
                         api_key = "OPENROUTER_API_KEY",
                         end_point = "https://openrouter.ai/api/v1/chat/completions",
-                        model = "deepseek/deepseek-v4-flash",
+                        model = "google/gemini-2.5-flash",
                         name = "Openrouter",
                         optional = {
                             max_tokens = 128,
