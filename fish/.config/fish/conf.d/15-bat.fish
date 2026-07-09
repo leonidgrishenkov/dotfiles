1e9b958eb681584cd86f598d091e55cba41a071e
Leonid Grishenkov <grishenkovleonid@proton.me>
2026-07-09 17:19:37 +0300

    fix: bat help page

--- fish/.config/fish/conf.d/15-bat.fish
+++ fish/.config/fish/conf.d/15-bat.fish
@@ -4,5 +4,6 @@
 set -gx MANPAGER "sh -c 'awk \"{ gsub(/\\x1B\\[[0-9;]*m/, \\\"\\\", \\\$0); gsub(/.\\x08/, \\\"\\\", \\\$0); print }\" | bat -p -lman'"
 
 # Pipe --help through bat for syntax highlighting
-abbr -a -- -h '-h 2>&1 | bat --language=help --style=plain'
-abbr -a -- --help '--help 2>&1 | bat --language=help --style=plain'
+# https://github.com/sharkdp/bat?tab=readme-ov-file#highlighting---help-messages
+abbr -a --position anywhere -- --help '--help | bat -plhelp'
+abbr -a --position anywhere -- -h '-h | bat -plhelp'
