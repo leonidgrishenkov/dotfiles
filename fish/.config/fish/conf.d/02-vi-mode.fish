6777987d1d808ff6a594098370b1bcdca86a87a5
Leonid Grishenkov <grishenkovleonid@proton.me>
2026-07-09 16:53:00 +0300

    add keybind to open cmdline buff in editor

--- fish/.config/fish/conf.d/02-vi-mode.fish
+++ fish/.config/fish/conf.d/02-vi-mode.fish
@@ -7,5 +7,8 @@ fish_vi_key_bindings
 # (bound in command mode to leave insert mode)
 bind -M insert jf "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 
+# Open command line buffer in $EDITOR (vi command mode: vv)
+bind -M default vv edit_command_buffer
+
 # Copy to system clipboard in visual mode
 bind -M visual y "fish_clipboard_copy"
