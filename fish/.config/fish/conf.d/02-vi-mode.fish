ef9f2ab186704b3036ed01499104c8689eabfa67
Leonid Grishenkov <grishenkovleonid@proton.me>
2026-07-10 21:10:48 +0300

    feat(fish): bind Ctrl+F to accept full autosuggestion

--- fish/.config/fish/conf.d/02-vi-mode.fish
+++ fish/.config/fish/conf.d/02-vi-mode.fish
@@ -3,6 +3,9 @@
 # Enable vi keybindings
 fish_vi_key_bindings
 
+# Accept the full autosuggestion line (Ctrl+F)
+bind -M insert \cf accept-autosuggestion
+
 # Custom escape binding: jf instead of Esc
 # (bound in command mode to leave insert mode)
 bind -M insert jf "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
