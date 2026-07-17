3c7ba43669f70a94e01c0a8ae5ffed09edbd526a
Leonid Grishenkov <grishenkovleonid@proton.me>
2026-07-17 13:37:25 +0300

    feat(fish): add a few abbrs

--- fish/.config/fish/conf.d/01-aliases.fish
+++ fish/.config/fish/conf.d/01-aliases.fish
@@ -1,12 +1,17 @@
 # vim: set filetype=fish:
 
 # === Shortcuts (expand inline via abbr) ===
-abbr --add k      kubectl
-abbr --add d      docker
-abbr --add t      terragrunt
-abbr --add g      git
-abbr --add v      $VISUAL
-abbr --add gp     gopass
+abbr --add k kubectl
+abbr --add d docker
+abbr --add t terragrunt
+abbr --add g git
+abbr --add v $VISUAL
+abbr --add gp gopass
+abbr --add l lazygit
 
-# === Modern alternatives ===
+# Include hidden/ignore files by default
+abbr --add rg "rg -."
+abbr --add fd "fd -IH"
+
+# === Eza ===
 alias ls="eza --icons=auto --group-directories-first --time-style=long-iso"
